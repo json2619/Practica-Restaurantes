0a1274efd0f013b9679a883740b573b36fc32e51
josemi <josemiguelf.19@hotmail.com>
2024-01-16 09:32:31 +0100

    Version 1.0 RestaurantManager

---
+++ Practica_JoseMiguel_Flores/js/RestaurantManager.js
@@ -0,0 +1,40 @@
+const RestaurantsManager = (function () {
+    // Creamos la instace de nuestro patron Singleton
+    let instance;
+
+    // Creamos el objeto RestaurantsManager
+    class RestaurantsManager {
+        // Creamos las variables privadas de nuestra clase
+        #name;
+        #categories = new Map();
+        #allergens = new Map();
+        #dishes = new Map();
+        #menus = new Map();
+        #restaurants = new Map();
+
+        constructor(name = "Restaurant Manager") {
+
+            this.#name = name;
+        }
+
+    }
+
+    // Función con la que crearemos la instancia 
+    // de nuestro patrón Singleton
+    function createInstance() {
+        const restaurantManager = new RestaurantsManager();
+        return restaurantManager;
+    }
+
+    return {
+        getInstance: function () {
+            // Si la instacia no existe llamamos a nuestra función de creación
+            if (!instance) {
+                instance = createInstance();
+            }
+
+            // Retornamos la instancia de nuestro patrón
+            return instance;
+        }
+    }
+})();
