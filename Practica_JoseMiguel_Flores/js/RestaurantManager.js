da72f7954f277c746b01c7b3606381683b0ff72a
josemi <josemiguelf.19@hotmail.com>
2024-01-16 09:35:29 +0100

    Version 1.3 RestaurantManager

--- Practica_JoseMiguel_Flores/js/RestaurantManager.js
+++ Practica_JoseMiguel_Flores/js/RestaurantManager.js
@@ -65,6 +65,67 @@ const RestaurantsManager = (function () {
             return this; // Retornar la instancia para encadenar
         }
 
+        addMenu(menu) {
+            if (!menu) {
+                throw new EmptyValueException();
+            }
+
+            if (this.#menus.has(menu.getName())) {
+                throw new RegisteredObjectException();
+            }
+
+            this.#menus.set(menu.getName(), {
+                newMenu: menu,
+                dishMenuArr: [],
+            });
+            return this;
+        }
+
+        removeMenu(menu) {
+            if (!this.#menus.has(menu.getName())) {
+                throw new NonRegisteredObjectException();
+            }
+
+            this.#menus.delete(menu.getName());
+            return this;
+        }
+
+        addAllergen(allergen) {
+            if (!allergen) {
+                throw new EmptyValueException();
+            }
+
+            if (this.#allergens.has(allergen.getName())) {
+                throw new RegisteredObjectException();
+            }
+
+            this.#allergens.set(allergen.getName(), allergen);
+            return this;
+        }
+
+        removeAllergen(allergen) {
+            if (!allergen) {
+                throw new EmptyValueException();
+            }
+
+            if (!this.#allergens.has(allergen.getName())) {
+                throw new NonRegisteredObjectException();
+            }
+
+            // Desasignar el alérgeno de todos los platos
+            this.#dishes.forEach(dish => {
+                const index = dish.dishAllergens.findIndex(alg => alg.getName() === allergen.getName());
+                if (index !== -1) {
+                    dish.dishAllergens.splice(index, 1);
+                }
+            });
+
+            // Eliminar el alérgeno del mapa de alérgenos
+            this.#allergens.delete(allergen.getName());
+
+            return this; // Retornar la instancia para encadenar
+        }
+
     }
 
     // Función con la que crearemos la instancia 
